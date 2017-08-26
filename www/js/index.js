d6ee06da9366f37f1aae7ff23022f445c00ee17c
Piotr Błachewicz <piotrblachewicz@MacBook-Pro-Konrad.local>
2017-08-26 16:32:58 +0200

    disable splash

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,6 @@ var app = {
     // Application Constructor
     initialize: function() {
         
-        navigator.splashscreen.show();
         if(localStorage.getItem('user_token')){
           document.querySelector('#myNavigator').setAttribute('page','page2.html');
         }
